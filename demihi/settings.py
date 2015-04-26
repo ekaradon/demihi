595064c8a3d76366f079eea3cf10ca073e995fd0
Denis Zimmermann <ekaradon@users.noreply.github.com>
2015-04-26 18:32:39 +0200

    hiding secret key from public repository

--- demihi/settings.py
+++ demihi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r580tmnzu^a8c0o_ygez36o+5@!b01*#hnpxd)+o(d(_a^_8px'
+SECRET_KEY = open(os.path.expanduser('~/.secret')).read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
