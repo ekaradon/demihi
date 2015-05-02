f63d2047cbbf80e47e0d8db8a7139a77ea875994
Denis Zimmermann <ekaradon@users.noreply.github.com>
2015-05-02 20:00:24 +0200

    specify explicitly environment used for production using wsgi

--- demihi/wsgi.py
+++ demihi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demihi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demihi.settings.production")
 
 application = get_wsgi_application()
